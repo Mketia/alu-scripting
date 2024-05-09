5f2154b21b07244e0e45791dc2025fd1808464f9
root <k.mukakalis1@alustudent.com>
2024-05-09 03:30:30 -0700

    8-textme.rb

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,7 +1,2 @@
 #!/usr/bin/env ruby
-#SENDER = ARGV[0].scan(/from:\+*\w*/).join[5..-1]
-#RECEIVER = ARGV[0].scan(/to:\+*\w*/).join[3..-1]
-#FLAGS = ARGV[0].scan(/flags:(.*?)\]/).join
-#
-#msg = SENDER + "," + RECEIVER + "," + FLAGS
-#puts msg
+'Feb 1 11:00:00 ip-10-0-0-11 mdr: 2016-02-01 11:00:00 Receive SMS [SMSC:SYBASE1] [SVC:] [ACT:] [BINF:] [FID:] [from:Google] [to:+16474951758] [flags:-1:0:-1:0:-1] [msg:127:This planet has - or rather had - a problem, which was this: most of the people on it were unhappy for pretty much of the time.] [udh:0:]'

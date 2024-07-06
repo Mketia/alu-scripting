4f8d8185148a7798373e87dcd0809fdb0e91180c
@Mketia <k.mukakalis1@alustudent.com>
2024-07-06 20:55:24 +0000

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json().get('data') \
-            .get('subscribers')
-    return 0
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'your_custom_user_agent'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
+
+# Example usage:
+print(number_of_subscribers("python")) 
+ # Replace "python" with any subreddit name
